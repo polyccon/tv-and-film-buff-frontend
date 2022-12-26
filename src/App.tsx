c4901acb472f1438eb24b2521e5a71ee6938f40d
Constantina Polycarpou <polyccon@gmail.com>
2022-12-26 20:12:37 +0000

    feat: adds react router and home view

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import Home from "./Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-2" style={{ marginTop: 40 }}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/Home/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import FILM from '../assets/film-image.png';
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={FILM} className="" alt="logo" />
+        <p>Choose your favourite films and series from</p>
+        <a
+          className="App-link"
+          href="https://www.imdb.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          IMDB
+        </a>
+      </header>
+    </div>
+  );
+}
