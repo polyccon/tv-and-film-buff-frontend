62a01b95fe15e8f0c963bd7c6a1b0205de3a4cbf
Constantina Polycarpou <polyccon@gmail.com>
2022-12-26 20:13:46 +0000

    feat: add navbar with home and films links

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,16 @@ import {
 } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import Home from "./Home";
+import Navbar from "./Navbar";
 
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="container mt-2" style={{ marginTop: 40 }}>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/home">
             <Home />
           </Route>
         </Switch>
---
+++ src/Navbar/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { NavbarLink } from "../components/NavbarLink.js";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <nav
+      className="navbar is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <a
+            role="button"
+            className={`navbar-burger burger ${isOpen && "is-active"}`}
+            aria-label="menu"
+            aria-expanded="false"
+            onClick={() => setOpen(!isOpen)}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div className={`navbar-menu ${isOpen && "is-active"}`}>
+          <div className="navbar-start">
+            <NavbarLink url="/home" navLinkClass="navbar-item">
+              Home
+            </NavbarLink>
+            <br />
+            <NavbarLink url="/myfilms" navLinkClass="navbar-item">
+              My Films
+            </NavbarLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
